249af3077bad5c4c5a4e97a9b3e35d9e210e8b4b
Howard Tyson <howard@howardtyson.com>
2016-09-02 18:27:26 -0700

    Stubbing out a consumer stream and test.

--- kafka.js
+++ kafka.js
@@ -1,6 +1,7 @@
 exports.HighLevelConsumer = require('./lib/highLevelConsumer');
 exports.HighLevelProducer = require('./lib/highLevelProducer');
 exports.Consumer = require('./lib/consumer');
+exports.ConsumerStream = require('./lib/consumerStream');
 exports.Producer = require('./lib/producer');
 exports.Client = require('./lib/client');
 exports.Offset = require('./lib/offset');
